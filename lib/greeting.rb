d4b049bf934f4fc8712f848dead5303ae9e64d9e
Cheng Vue <cvu3@ucsc.edu>
2018-11-15 00:53:56 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-  greeting_message = "Hello #{name}. It's nice to meet you."
-  puts greeting_message.strip
+  puts "Hello #{name}. It's nice to meet you."
 end
